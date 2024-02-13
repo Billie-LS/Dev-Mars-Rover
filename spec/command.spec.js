2015f66ade2843448b10cde58b23df69fd13f6d6
Billie-LS <lskylizard@yahoo.com>
2024-02-12 23:48:32 -0600

    Command object commenting tests

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -1,11 +1,13 @@
+// Import the Command class from the command.js file
 const Command = require("../command.js");
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
+// describe Command class unit tests
 describe("Command class", function () {
   // Test 1
-  // TODO: description here
+  // Throws error if command type NOT passed into constructor as first parameter
   it("throws error if command type is NOT passed into constructor as the first parameter", function () {
     expect(function () {
       new Command();
@@ -13,9 +15,11 @@ describe("Command class", function () {
   });
 
   // Test 2
-  // TODO: description here
+  // constructor sets command type
   it("constructor sets command type", function () {
+    // new Command instance with commandType "MOVE"
     const command = new Command("MOVE");
+    // expect commandType property of command object is "MOVE"
     expect(command.commandType).toEqual("MOVE");
   });
 
@@ -24,9 +28,11 @@ describe("Command class", function () {
   // });
 
   // Test 3
-  // TODO: description here
+  // constructor sets value passed as 2nd argument
   it("constructor sets a value passed in as the 2nd argument", function () {
+    // new Command instance with commandType "MOVE" and value 7
     const command = new Command("MOVE", 7);
+    // expect value property of command object to be 7
     expect(command.value).toEqual(7);
   });
 });
