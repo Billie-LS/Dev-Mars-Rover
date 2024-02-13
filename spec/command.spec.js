596dbe6e76b9602c60ab4e6a0de89744514c331f
Billie-LS <lskylizard@yahoo.com>
2024-02-12 23:36:40 -0600

    message test/spec 4,5,6 and module

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -1,15 +1,20 @@
-const Command = require('../command.js');
+const Command = require("../command.js");
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
-describe('Command class', function() {
-
-  it('throws error if command type is NOT passed into constructor as the first parameter', function() {
-    expect( function() { new Command();}).toThrow(new Error('Command type required.'));
+describe("Command class", function () {
+  // Test 1
+  // TODO: description here
+  it("throws error if command type is NOT passed into constructor as the first parameter", function () {
+    expect(function () {
+      new Command();
+    }).toThrow(new Error("Command type required."));
   });
 
-  it('constructor sets command type', function() {
+  // Test 2
+  // TODO: description here
+  it("constructor sets command type", function () {
     const command = new Command("MOVE");
     expect(command.commandType).toEqual("MOVE");
   });
@@ -18,11 +23,10 @@ describe('Command class', function() {
   //   expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   // });
 
-  it('constructor sets a value passed in as the 2nd argument', function() {
+  // Test 3
+  // TODO: description here
+  it("constructor sets a value passed in as the 2nd argument", function () {
     const command = new Command("MOVE", 7);
     expect(command.value).toEqual(7);
   });
-
-
-
 });
--- spec/message.spec.js
+++ spec/message.spec.js
@@ -1,9 +1,51 @@
-const Message = require('../message.js');
-const Command = require('../command.js');
+const Message = require("../message.js");
+const Command = require("../command.js");
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
-describe("Message class", function() {
+describe("Message class", function () {
+  // Test 4
+  // TODO: description here
+  it("throws error if a name is NOT passed into the constructor as the first parameter", function () {
+    expect(function () {
+      new Message();
+    }).toThrow(new Error("Message name required."));
+  });
 
+  // Test 5
+  // TODO: description here
+  it("constructor sets name", function () {
+    const message = new Message("Test message with two commands");
+    expect(message.name).toEqual("Test message with two commands");
+  });
+
+  // Test 6
+  // TODO: description here
+  it("contains a commands array passed into the constructor as the 2nd argument", function () {
+    // const message = new Message("Test message with two commands", 7);
+
+    const commands = [
+      new Command("MODE_CHANGE", "LOW_POWER"),
+      new Command("STATUS_CHECK"),
+    ];
+    const message = new Message("Test message with two commands", commands);
+    expect(message.name).toEqual("Test message with two commands", commands);
+  });
 });
+
+//     it("throws error if command type is NOT passed into constructor as the first parameter", function () {
+//       expect(function () {
+//         new Command();
+//       }).toThrow(new Error("Command type required."));
+//     });
+
+//     it("constructor sets command type", function () {
+//       const command = new Command("MOVE");
+//       expect(command.commandType).toEqual("MOVE");
+//     });
+
+//     it("constructor sets a value passed in as the 2nd argument", function () {
+//       const command = new Command("MOVE", 7);
+//       expect(command.value).toEqual(7);
+//     });
