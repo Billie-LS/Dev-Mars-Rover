e48d5a5ae52718f71f1e53d92843ee26506f0b57
Billie-LS <lskylizard@yahoo.com>
2024-02-17 14:18:15 -0600

    rover - message

---
+++ message copy.js
@@ -0,0 +1,12 @@
+/*
+Message Class Description
+Message class builds object with two properties:
+constructor(name, commands)
+name is a string that is the name of the message.
+commands is an array of Command objects.
+*/
+class Message {
+  // Write code here!
+}
+
+module.exports = Message;
---
+++ spec/message.spec copy.js
@@ -0,0 +1,13 @@
+const Message = require("../message.js");
+const Command = require("../command.js");
+
+// NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
+//       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
+/*
+Message Class Description
+Message class builds object with two properties:
+constructor(name, commands)
+name is a string that is the name of the message.
+commands is an array of Command objects.
+*/
+describe("Message class", function () {});
