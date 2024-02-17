2a4eae593976be67725c51528091cd811075950b
Billie-LS <lskylizard@yahoo.com>
2024-02-17 10:10:34 -0600

    rover - Command

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -5,7 +5,7 @@ const Command = require("../command.js");
 
 describe("Command class", function () {
   // Test 1
-  it("throws error if command type is NOT passed into constructor as the first parameter", function () {
+  it("throws error if command type is NOT passed into constructor as the first parameter", () => {
     expect(function () {
       new Command();
     }).toThrow(new Error("Command type required."));
