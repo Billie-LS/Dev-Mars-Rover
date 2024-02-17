d207b6bf087e21e85396027f965ec7018a209b3f
Billie-LS <lskylizard@yahoo.com>
2024-02-17 10:07:49 -0600

    rover - Command

---
+++ command copy.js
@@ -0,0 +1,18 @@
+/*
+This class builds an object with two properties: 
+constructor(commandType, value)
+commandType is a string that represents the type of command:
+MODE_CHANGE, MOVE, or STATUS_CHECK.
+value is a value related to the type of command.
+*/
+class Command {
+  constructor(commandType, value) {
+    this.commandType = commandType;
+    if (!commandType) {
+      throw Error("Command type required.");
+    }
+    this.value = value;
+  }
+}
+
+module.exports = Command;
--- spec/command.spec copy.js
+++ spec/command.spec copy.js
@@ -1,27 +1,22 @@
 const Command = require("../command.js");
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
-//       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
+// do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Command class", function () {
-  // Test 1
   it("throws error if command type is NOT passed into constructor as the first parameter", function () {
     expect(function () {
       new Command();
     }).toThrow(new Error("Command type required."));
   });
 
-  // Test 2
   it("constructor sets command type", () => {
-    let command = new Command("MOVE");
-    expect(command.commandType).toEqual("MOVE");
+    let command = new Command("Move");
+    expect(command.commandType).toBe("Move");
   });
 
-  // Test 3
   it("constructor sets a value passed in as the 2nd argument", () => {
-    let command = new Command("MOVE", 7);
-    expect(command.value).toEqual(7);
+    let command = new Command("Move", 12000);
+    expect(command.value).toEqual(12000);
   });
 });
-
-//
