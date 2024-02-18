f34b31bf4024edd0255bf3d03eb8366922306d1d
Billie-LS <lskylizard@yahoo.com>
2024-02-17 20:28:47 -0600

    rover - command

--- command.js
+++ command.js
@@ -7,12 +7,19 @@ value is a value related to the type of command.
 */
 class Command {
   constructor(commandType, value) {
+    if (!commandType) throw Error("Command type required.");
     this.commandType = commandType;
-    if (!commandType) {
-      throw Error("Command type required.");
-    }
     this.value = value;
   }
 }
 
+// class Command {
+//   constructor(commandType, value) {
+//     this.commandType = commandType;
+//     if (!commandType) {
+//       throw Error("Command type required.");
+//     }
+//     this.value = value;
+//   }
+// }
 module.exports = Command;
