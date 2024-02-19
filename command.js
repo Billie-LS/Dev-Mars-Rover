d6376de210807aba46e4caeabe5147a4f596bc14
Billie-LS <lskylizard@yahoo.com>
2024-02-19 17:32:43 -0600

    started cleanup of extra sandbox files

--- command.js
+++ command.js
@@ -5,10 +5,17 @@ commandType is a string that represents the type of command:
 MODE_CHANGE, MOVE, or STATUS_CHECK.
 value is a value related to the type of command.
 */
+// class named Command
 class Command {
+  // constructor with two properties, commandType and value
   constructor(commandType, value) {
+    // logic check for commandType is input or throw an error
     if (!commandType) throw Error("Command type required.");
+
+    // constructor set commandType property to commandType argument provided
     this.commandType = commandType;
+
+    // constructor set value property to value argument provided
     this.value = value;
   }
 }
@@ -22,4 +29,6 @@ class Command {
 //     this.value = value;
 //   }
 // }
+
+// export Command class as Command module
 module.exports = Command;
--- message.js
+++ message.js
@@ -5,10 +5,18 @@ constructor(name, commands)
 name is a string that is the name of the message.
 commands is an array of Command objects.
 */
+
+// class named Command
 class Message {
+  // constructor with two properties, name and commands, commands default to empty array
   constructor(name, commands = []) {
+    // logic check for name is input or throw an error
     if (!name) throw Error("Message name required.");
+
+    // constructor set name property to name argument provided
     this.name = name;
+
+    // constructor set commands property to commands argument provided
     this.commands = commands;
   }
 }
@@ -23,4 +31,5 @@ class Message {
 //   }
 // }
 
+// export Message class as Message module
 module.exports = Message;
