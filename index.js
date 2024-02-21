d367eda7f0ebac994c4d7180370d49be441bc499
Billie-LS <lskylizard@yahoo.com>
2024-02-20 18:11:53 -0600

    rover class super spec test

--- index.js
+++ index.js
@@ -5,14 +5,43 @@ const Message = require("./message.js");
 // import Command class from command.js
 const Command = require("./command.js");
 
-// generate Rover object with specified position
-const rover = new Rover(98382); // pass 98382 as the rover's position.
+// it("Responds to TA message & commands", function() {
+//   let rover = new Rover(100);
+//   let commands = [
+//      new Command('MOVE', 4321),
+//      new Command('STATUS_CHECK'),
+//      new Command('MODE_CHANGE', 'LOW_POWER'),
+//      new Command('MOVE', 3579),
+//      new Command('STATUS_CHECK')
+//   ];
+//   let message = new Message('TA power', commands);
+//   let response = rover.receiveMessage(message);
+//   expect(response.message).toEqual('TA power');
+//   expect(response.results[0].completed).toBeTruthy();
+//   expect(response.results[1].roverStatus.position).toEqual(4321);
+//   expect(response.results[2].completed).toBeTruthy();
+//   expect(response.results[3].completed).toBeFalsy();
+//   expect(response.results[4].roverStatus.position).toEqual(4321);
+//   expect(response.results[4].roverStatus.mode).toEqual('LOW_POWER');
+//   expect(response.results[4].roverStatus.generatorWatts).toEqual(110);
+//  });
 
-// generate Message object with specified name and array of two commands
-const message = new Message("Test message name", [
+// generate Rover object with specified position
+// const rover = new Rover(98382); // pass 98382 as the rover's position.
+let rover = new Rover(100);
+let commands = [
+  new Command("MOVE", 4321),
   new Command("STATUS_CHECK"),
-  new Command("MOVE", 100),
-]);
+  new Command("MODE_CHANGE", "LOW_POWER"),
+  new Command("MOVE", 3579),
+  new Command("STATUS_CHECK"),
+];
+// generate Message object with specified name and array of two commands
+// const message = new Message("Test message name", [
+//   new Command("STATUS_CHECK"),
+//   new Command("MOVE", 100),
+// ]);
+let message = new Message("TA power", commands);
 
 // call receiveMessage method on Rover object with message
 const response = rover.receiveMessage(message);
